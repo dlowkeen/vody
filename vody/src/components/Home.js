db33e1b3ea0721318fc457cde30da5035607bcd1
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-04-12 17:15:17 -0700

    FIXED IT, pulling data onto DOM from OMBD

--- vody/src/components/Home.js
+++ vody/src/components/Home.js
@@ -8,21 +8,20 @@ export default class Home extends Component {
     let title = "space+jam";
     let queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=40e9cece";
     axios.get(queryURL).then(response => {
-      console.log("response", response);
       let movie = response.data;
       this.setState({
-        movies: movie
+        movie: movie
       });
     });
   }
 
   renderContent() {
-    // const movie = this.state.movie ? this.state.movie.title : null;
-    console.log(this.state);
+    const movie = this.state ? this.state.movie.Title : null;
+    console.log(movie);
     if (this.state) {
       return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-          <Text>movie to display here!</Text>
+          <Text>{movie}</Text>
         </View>
       );
     } 
