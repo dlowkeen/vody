2b034958e1794401ec07d1dd32aa926d4803f886
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-04-13 21:25:45 -0700

    rendering list of movies

--- vody/src/components/home/Home.js
+++ vody/src/components/home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, ScrollView } from 'react-native';
 import { Container } from 'native-base';
 import axios from 'axios';
 import MovieCard from "../common/MovieCard";
@@ -9,8 +9,7 @@ export default class Home extends Component {
   componentWillMount() {
     // To do: remove and store in prod config variables 
     let APIKey = "718190bc3e37096f5f6a3adfdeb9abaa";
-    // let queryURL = "https://www.omdbapi.com/?t=" + title + "&y=" + year + "&plot=short&apikey=40e9cece";
-    let queryURL = "https://api.themoviedb.org/3/movie/76341?api_key=" + APIKey;
+    let queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=" + APIKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
     axios.get(queryURL).then(response => {
       let movie = response.data;
       this.setState({
@@ -20,30 +19,30 @@ export default class Home extends Component {
   }
 
   renderContent() {
-    const movie = this.state ? this.state.movie : null;
+    const movie = this.state ? this.state.movie.results : null;
     console.log(movie);
-    let moviePath;
-    if (movie) {
-      moviePath = "https://image.tmdb.org/t/p/w500/" + movie.poster_path;
-    }
-    console.log(moviePath);
+    let moviePath = "https://image.tmdb.org/t/p/w500/"
     if (this.state) {
-      return <Container>
-          <MovieCard 
-            Poster={moviePath}
-            release_date={movie.release_date}
-            vote_average={movie.vote_average}
-            overview={movie.overview}
-          />
-        </Container>;
-      } 
+      const movieList = movie.map(movie=> {
+        console.log(movie);
+        return <Container>
+            <MovieCard 
+              Poster={moviePath + movie.poster_path}
+              release_date={movie.release_date}
+              vote_average={movie.vote_average}
+              overview={movie.overview}
+            />
+          </Container>;
+      });
+      return movieList;
+    } 
   }
 
   render() {
     return (
-      <Container>
+      <ScrollView>
         {this.renderContent()}
-      </Container>
+      </ScrollView>
     );
   }
 }
