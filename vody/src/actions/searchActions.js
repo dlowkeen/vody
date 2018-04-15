9a3a2025d5990c871b6bdad1a5d87cb09ee87681
Donovan <donovanlowkeen@Donovans-MBP.fios-router.home>
2018-04-14 18:41:44 -0700

    making correct axios call, possible unahndled promise rejection

--- vody/src/actions/searchActions.js
+++ vody/src/actions/searchActions.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 import { FETCH_MOVIE_DATA } from "./types";
 
-export const fetchMovieData = data => async dispatch => {
+export const fetchMovieData = data => dispatch => {
   console.log("ACTION!", data);
   // // To do: remove and store in prod config variables 
   let APIKey = "718190bc3e37096f5f6a3adfdeb9abaa";
   const { title, year } = data;
   let queryURL = "https://api.themoviedb.org/3/search/movie/?api_key=" + APIKey + "&language=en-US&query=" + title + "&page=1&include_adult=false";
-  const res = await axios.get(queryURL, data);
-  console.log(res.data);
-  dispatch({ 
-    type: FETCH_MOVIE_DATA, 
-    payload: res.data 
-  });
+  console.log("queryURL", queryURL);
+  axios.get(queryURL).then(res => {
+    console.log(res);
+    dispatch({ 
+      type: FETCH_MOVIE_DATA, 
+      payload: res.data 
+    });
+  }); 
 };
