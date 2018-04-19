9c7e0d9a85fddc434a20a62783b14ecb501abd2e
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-04-18 20:30:00 -0700

    minor styling to login and home page

--- vody/src/components/login/Login.js
+++ vody/src/components/login/Login.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Text, View, TouchableOpacity, TouchableHighlight, StyleSheet, Image } from 'react-native';
 import FBSDK, { LoginManager } from 'react-native-fbsdk';
 import * as actions from '../../actions';
+const FBLogo = require("../../assets/images/FB-f-Logo__white_144.png");
 
 class Login extends Component {
   constructor(props) {
@@ -41,110 +42,89 @@ class Login extends Component {
 
   render() {
     this.isUserLoggedIn();
-    return (
-      <View style={{ flex: 1, backgroundColor: '#f37a81' }}>
+    return <View style={{ flex: 1, backgroundColor: "#d122ff" }}>
         <View style={styles.container}>
-          {/* <ImageBackground
-            style={{ flex: 1, justifyContent: 'center' }}
-            source={pirateShipGIF}
-          > */}
-            <Text style={styles.titleFont}>
-              ARgo
-            </Text>
-            <TouchableHighlight
-              style={styles.container2}
-              onPress={this.facebookAuth}
-              underlayColor='transparent'
-            >
-              <View style={styles.FBLoginButton}>
-                {/* <Image style={styles.FBLogo} source={FBLogo} /> */}
-                <Text
-                  style={styles.FBLoginButtonText}
-                  numberOfLines={1}
-                >
-                  Continue with Facebook
-                </Text>
-              </View>
-            </TouchableHighlight>
+          <Text style={styles.titleFont}>
+            VODY
+          </Text>
+          {/* <Text style={styles.titleFont}>VODY</Text> */}
+          <TouchableHighlight style={styles.container2} onPress={this.facebookAuth} underlayColor="transparent">
+            <View style={styles.FBLoginButton}>
+              <Image style={styles.FBLogo} source={FBLogo} />
+              <Text style={styles.FBLoginButtonText} numberOfLines={1}>
+                Continue with Facebook
+              </Text>
+            </View>
+          </TouchableHighlight>
 
-            <Text
-              style={styles.designer}
-              numberOfLines={1}
-            >Designed by Fabien Rousseau</Text>
-          {/* </ImageBackground> */}
+          <Text style={styles.designer} numberOfLines={1}>
+            Built by Donovan Lowkeen
+          </Text>
         </View>
-      </View>
-    );
+      </View>;
   }
 }
 
 const styles = StyleSheet.create({
-  spinner: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
   container: {
     top: 0,
     left: 0,
-    position: 'absolute',
-    width: '100%',
-    height: '80%'
+    position: "absolute",
+    width: "100%",
+    height: "80%"
   },
   titleFont: {
-    backgroundColor: 'transparent',
-    // fontFamily: 'IM Fell English',
-    fontStyle: 'italic',
+    fontFamily: "Helvetica",
     fontSize: 45,
-    textAlign: 'center',
-    paddingTop: 570,
-    margin: 20,
-    color: '#FFFFFF'
+    textAlign: "center",
+    paddingTop: 200,
+    margin: 50,
+    fontWeight: "700",
+    color: "#FFFFFF"
   },
   container2: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
   },
   FBLoginButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     height: 30,
     width: 200,
     paddingLeft: 2,
-    backgroundColor: 'rgb(66,93,174)',
+    backgroundColor: "rgb(66,93,174)",
     borderRadius: 3,
     borderWidth: 1.5,
-    borderColor: 'rgb(66,93,174)',
-    shadowColor: '#000000',
+    borderColor: "rgb(66,93,174)",
+    shadowColor: "#000000",
     shadowOpacity: 0.8,
     shadowRadius: 2,
     shadowOffset: {
       height: 1,
       width: 0
-    },
+    }
   },
   FBLoginButtonText: {
-    color: 'white',
-    fontWeight: '600',
-    fontFamily: 'Helvetica neue',
+    color: "white",
+    fontWeight: "600",
+    fontFamily: "Helvetica neue",
     fontSize: 14.2,
-    marginLeft: 18,
+    marginLeft: 18
   },
   FBLogo: {
-    position: 'absolute',
+    position: "absolute",
     height: 14,
     width: 14,
     left: 7,
-    top: 7,
+    top: 7
   },
   designer: {
-    // fontFamily: 'Lato-Regular',
     fontSize: 10,
-    textAlign: 'center',
+    textAlign: "center",
     bottom: -70,
-    color: '#C8243B'
+    color: "#ffffff"
   }
 });
 
